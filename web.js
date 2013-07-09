81a270a2357a5ddf2601db0e85e2dedaedd8c06e
ernesto lopez <ernesto@ideosity.com>
2013-07-09 17:23:29 -0500

    trying to serve everything as public/static

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require("express");
 var app = express();
 app.use(express.logger());
+app.use(express.static(__dirname + '/'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.sendfile(__dirname + '/game.htm');
 });
 
 var port = process.env.PORT || 5000;
