7f33f48b51c5afad3636a3901b1cbdbff7467275
ernesto lopez <ernesto@ideosity.com>
2013-07-09 17:51:30 -0500

    added post ack on root route

--- web.js
+++ web.js
@@ -3,8 +3,14 @@ var app = express();
 app.use(express.logger());
 app.use(express.static(__dirname + '/'));
 
-app.get('/', function(request, response) {
+app.get('/', function (request, response) {
   response.sendfile(__dirname + '/game-fb.htm');
+});
+
+app.post('/', function (request, response) {
+	// facebook sends a post here...
+	// example: /?fb_source=bookmark_apps&amp;ref=bookmarks&amp;count=0&amp;fb_bmpos=3_0
+	response.send('acknowledged');
 });
 
 var port = process.env.PORT || 5000;
