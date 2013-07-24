23edf0f1bbd88c5666a3f72b77dd46e959ea9986
readonlychild <neto.lopez@gmail.com>
2013-07-24 00:04:41 -0500

    adding Leaderboard

---
+++ Leaderboard.js
@@ -0,0 +1,96 @@
+var https = require('https');
+
+var _context = {};
+
+exports.init = function (options) {
+	_context.appCode = options.appCode || '_default';
+	_context.dbName = options.dbName || 'gamestore';
+	_context.collName = options.collName || 'leaderboard';
+	_context._apikey = options._apikey || 'SRjT6dtJFin5RZhx5qUIxYqLdDKRDskF';
+	_context.serverHost = 'api.mongolab.com';
+	_context.serverPath = '/api/1/databases/' + _context.dbName + '/collections/' + _context.collName + '/';
+	_context.authorizedHosts = [
+		'127.0.0.1'
+	];
+};
+
+exports.getLeaders = function (boardCode, callback) {
+	var bcode = boardCode || _context.appCode;
+	var p = _context.serverPath + '?apiKey=' + _context._apikey;
+	// &q={appCode:"test.board"}&s={score:-1}&l=10
+	p += "&q={appCode:\"" + bcode + "\"}&s={score:-1}&l=10";
+	var options = {
+		hostname: _context.serverHost,
+		host: _context.serverHost,
+		method: 'GET',
+		path: p
+	};
+	console.log('getLeaders>', options.hostname, p);
+	var responseText = '';
+	var req = https.request(options, function (res) {
+		console.log('requesting.h1>');
+		res.on('data', function (chunk) {
+			responseText += chunk;
+		});
+		res.on('end', function () {
+			//console.log('lb.getLeaders', responseText);
+			if (callback) callback(JSON.parse(responseText));
+		});
+	});
+	req.on('error', function (e) {
+		console.log('req.err>', e);
+	});
+	req.end();
+};
+
+
+exports.addEntry = function (entry, callback) {
+
+	entry.appCode = entry.appCode || _context.appCode;
+	entry.created = new Date().toJSON();
+
+	var boardEntry = JSON.stringify(entry);
+
+	console.log('addE>', boardEntry);
+
+	var headers = {
+		//'Accept': '*',
+		'Content-Type': 'application/json',
+		'Content-Length': boardEntry.length //,
+		//'Connection': ''
+	};
+
+	var options = {
+		hostname: _context.serverHost,
+		method: 'POST',
+		path: _context.serverPath + '?apiKey=' + _context._apikey,
+		headers: headers
+	};
+
+	var responseString = '';
+
+	var mongoreq = https.request(options, function (mres) {
+		
+		mres.setEncoding('utf-8');
+
+		console.log('statusCode: ', mres.statusCode);
+		console.log('headers: ', mres.headers);
+
+		
+		mres.on('data', function (chunk) {
+			responseString += chunk;
+		});
+
+		mres.on('end', function () {
+			console.log(responseString);
+			if (callback) callback(responseString);
+		});
+
+	});
+	
+	mongoreq.write(boardEntry);
+	console.log(boardEntry);
+	mongoreq.end();
+	mongoreq.on('error', function (e) { console.log('**ERROR* ', e) });
+	
+};
--- web.js
+++ web.js
@@ -1,19 +1,47 @@
-var express = require("express");
+var https = require('https');
+var express = require('express');
+
+var leaderboard = require('./Leaderboard');
+leaderboard.init({appCode:'test.board'});
+
 var app = express();
-app.use(express.logger());
-app.use(express.static(__dirname + '/'));
 
-app.get('/', function (request, response) {
-  response.sendfile(__dirname + '/game-fb.htm');
+app.get('/info', function (req, res) {
+	var body = '<h1>Information</h1>';
+	body += "req.hostname = [" + req.host + "]";
+	//console.log(req);
+	//res.setHeader('Content-Type', 'text/plain');
+	//res.setHeader('Content-Length', body.length);
+	
+	leaderboard.getLeaders(false, function (data) {
+		console.log('from.Info>', data);
+	});
+	res.end(body);
+	//res.send('Hello World');
 });
 
-app.post('/', function (request, response) {
-	// facebook sends a post here...
-	// example: /?fb_source=bookmark_apps&amp;ref=bookmarks&amp;count=0&amp;fb_bmpos=3_0
-	response.sendfile(__dirname + '/game-fb.htm');
+app.get('/leaderboard/add/:board/:userId/:score', function (req, res) {
+	
+	leaderboard.addEntry({
+		userId: req.params.userId,
+		score: parseFloat(req.params.score),
+		appCode: req.params.board
+	}, function (data) {
+		console.log('from.add-api>', data);
+	});
+
+	res.end(':D finished add-api!');
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.get('/leaderboard/:code', function (req, res) {
+
+	leaderboard.getLeaders(req.params.code, function (data) {
+		console.log('getLeaders.result', data);
+		res.write(JSON.stringify(data));
+		res.end();
+	});
+	//res.end();
 });
+
+app.listen(3000);
+console.log('Listening on port 3000');
